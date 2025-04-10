9820dfbf6a25d1adf72d5b0b5070011c9b5fe064
Erik Suris <87949031+erikvsuris@users.noreply.github.com>
2025-04-10 23:18:39 +0000

    Class exercise finished

--- springboot/src/main/java/br/pucrs/erikvsuris/springboot/ExemploController.java
+++ springboot/src/main/java/br/pucrs/erikvsuris/springboot/ExemploController.java
@@ -4,8 +4,14 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/biblioteca")
@@ -55,5 +61,61 @@ public class ExemploController {
         }
         return autores;
     }
+
+    // Query String
+    @GetMapping("/livrosautor")
+    public List<Livro> getLivrosDoAutor(@RequestParam(value = "autor") String autor) {
+        return livros.stream()
+            .filter(livro->livro.getAutor().equals(autor))
+            .toList();
+    }
+
+    @GetMapping("/livrosautorano/{autor}/ano/{ano}")
+    public List<Livro> getMethodName(@PathVariable(value = "autor") String autor,
+                                @PathVariable(value = "ano") int ano) {
+        return livros.stream()
+            .filter(livro->livro.getAutor().equals(autor))
+            .filter(livro->livro.getAno() == ano)
+            .toList();
+    }
+    
+
+    @PostMapping("/novolivro")
+    public boolean addLivro(@RequestBody final Livro novoLivro) {
+        return livros.add(novoLivro);
+    }
+
+    @GetMapping("/livrotitulo/{titulo}")
+    public ResponseEntity<Livro> getLivroTitulo(@PathVariable("titulo") String titulo) {
+        Livro resp = livros.stream()
+            .filter(livro->livro.getTitulo().equals(titulo))
+            .findFirst()
+            .orElse(null);
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(resp);
+    }
+
+/* 
+ * Path parameters:
+    3. Crie um endpoint para /livrosautorano/{autor}/ano/{ano} que trate um ‘path parameters’.
+    4. No Postman crie uma requisição para testar este endpoint com /livrosautorano/Pedro da
+    Silva/ano/2024
+    • Body data:
+    5. Crie um endpoint para /novolivro que trate um ‘body data’.
+    6. No Postman crie uma requisição POST para testar este endpoint com os dados em JSON:
+    {
+    "id":"160",
+    "titulo":"Postman",
+    "autor":"Joana Moura",
+    "ano": "2024"
+    }
+    7. Depois de executar, execute novamente o teste de /biblioteca/livros, para ver se o livro foi
+    inserido.
+    • Response Entity:
+    8. Crie um endpoint para /livrotitulo/{titulo} que retorna uma ‘ResponseEntity’.
+    9. No Postman crie uma requisição para testar este endpoint com /livrotitulo/Aprendendo
+    Java
+ */
 
 }
